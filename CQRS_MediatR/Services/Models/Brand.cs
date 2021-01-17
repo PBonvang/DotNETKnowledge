edfa2651ec8017336fe0dd3972d293d96adf7ba4
PB <Peter.bonvang@gmail.com>
2021-01-17 20:57:42 +0000

    CQRS_MediatR: Brand model added to services;

---
+++ CQRS_MediatR/Services/Models/Brand.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Services.Models
+{
+    public class BrandDetail
+    {}
+
+    public class BrandOverview
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+    }
+}
