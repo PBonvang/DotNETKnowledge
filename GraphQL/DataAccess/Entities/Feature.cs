97b758c25d20449f60e3734b51244fe18d2e5b38
PBonvang <peter.bonvang@gmail.com>
2021-01-31 15:53:25 +0000

    GraphQL: Nullability added;

--- GraphQL/DataAccess/Entities/Feature.cs
+++ GraphQL/DataAccess/Entities/Feature.cs
@@ -7,7 +7,7 @@ namespace DataAccess.Entities
     {
         public Guid Id { get; set; }
         [Required]
-        public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
     }
 }
